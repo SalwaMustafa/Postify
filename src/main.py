358fae332575ee55e82a7ec3b92385ab755435ab
SalwaMustafa <salwa2532003@gmail.com>
2025-08-30 23:48:35 +0300

    run the app

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from routes import Upload_text, Upload_settings
+from routes import Upload_text, Upload_settings, Upload_voice
 
 app = FastAPI()
 
 app.include_router(Upload_text.upload_text_router)
 app.include_router(Upload_settings.settings_router)
+app.include_router(Upload_voice.upload_voice_router)
