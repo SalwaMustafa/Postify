9db5b6ee893ef352f9cc6c3b207d3d56cdc11c6e
Rawan Elmoafy <rawanelmoafy@gmail.com>
2025-09-01 01:48:39 +0300

    save user description in database

--- src/routes/Upload_text.py
+++ src/routes/Upload_text.py
@@ -1,18 +1,23 @@
-from fastapi import  APIRouter
+from fastapi import  APIRouter, Request
 from .schemes.text import TextInput
-from models import storage
 from controllers import TextController
+from models.enums import DataBaseEnum
 
 upload_text_router = APIRouter()
 
 @upload_text_router.post("/upload/text")
-async def upload_text(data: TextInput):
+async def upload_text(data: TextInput, request:Request):
 
     validate_text = TextController()
     signal , message = validate_text.validate_input(data.user_id, data.content)
-    storage.text_inputs[data.user_id] = data.content
+
+    if signal:
+        collection = request.app.db_client[DataBaseEnum.TEXT_COLLECTION.value] 
+        result = await collection.insert_one(data.dict())
+        return{ "inserted_id": str(result.inserted_id)}
+
     return {
         "signal" : signal,
-        "message" : message,
-        "received_text": data.content
+        "message" : message
         }
+
