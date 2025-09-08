fba5ff3a65815f28d644ab54b03a2ec8bc477368
SalwaMustafa <salwa2532003@gmail.com>
2025-09-08 22:17:29 +0300

    run the app

---
+++ src/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from helpers.config import get_settings
+from pymongo import MongoClient
+from langgraph.checkpoint.mongodb import MongoDBSaver
+from enums.DataBaseEnum import DataBaseEnum
+from chatbots.generate_chat import get_graph  
+from events import generate_events
+
+settings = get_settings()
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup_event():
+    app.mongo_conn = MongoClient(settings.MONGODB_URL)
+    app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
+    app.memory_generate = MongoDBSaver(app.db_client[DataBaseEnum.GENERATE_COLLECTION.value])
+    app.graph = get_graph(app)
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    app.mongo_conn.close()
+
+
+app.include_router(generate_events.api)
