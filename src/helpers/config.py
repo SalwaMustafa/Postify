ac232111720350a4343238afe55e33e310fe1abc
SalwaMustafa <salwa2532003@gmail.com>
2025-09-08 22:16:08 +0300

    save database url and api key

---
+++ src/helpers/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+
+    APP_NAME : str
+    APP_VERSION : str
+    MONGODB_URL : str
+    MONGODB_DATABASE : str
+    GOOGLE_API_KEY: str
+    MODEL_NAME: str
+
+    
+    class Config:
+        env_file = '.env'
+
+
+
+def get_settings():
+    return Settings()
