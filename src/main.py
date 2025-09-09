ee708bec4eb76195f3790cefcb0634c27426b148
SalwaMustafa <salwa2532003@gmail.com>
2025-09-10 02:33:07 +0300

    run the app

--- src/main.py
+++ src/main.py
@@ -5,10 +5,12 @@ from langgraph.checkpoint.mongodb import MongoDBSaver
 from enums.DataBaseEnum import DataBaseEnum
 from chatbots.generate_chat import get_graph  
 from events import generate_events
+from events.generate_events import init_socket
+
 
 settings = get_settings()
 app = FastAPI()
-
+init_socket(app)
 
 @app.on_event("startup")
 async def startup_event():
@@ -16,10 +18,10 @@ async def startup_event():
     app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
     app.memory_generate = MongoDBSaver(app.db_client[DataBaseEnum.GENERATE_COLLECTION.value])
     app.graph = get_graph(app)
+    
 
 @app.on_event("shutdown")
 async def shutdown_event():
     app.mongo_conn.close()
 
 
-app.include_router(generate_events.api)
